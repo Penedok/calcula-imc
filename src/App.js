4e9c9a1eb9528e5d74c73cd1bb16a8b0502b869a
Karen Penedo <penedok12@gmail.com>
2023-05-12 22:59:03 -0400

    Adicionando o Header na página

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 
-import './App.css';
+
+import styled from 'styled-components'
+import Titulo from './componentes/titulo';
+import Frase from './componentes/frase';
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(180deg, #BFDAE6 0%, #FCF0DD 50%, #FACF9D 100%)
+
+`
+const Header = styled.header`
+  display:flex;
+  justify-content:space-around;
+  padding: 1em;
+
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    < AppContainer>
+      <Header>
+        <Titulo/>
+        <Frase/>
+      </Header>
+    </AppContainer>
   );
 }
 
